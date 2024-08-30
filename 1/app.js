56556cdc52913f74627a680febbdbe91f160a95e
Menza <menzamenza90@gmail.com>
2024-08-30 13:52:42 +0000

    ledoc 1

--- 1/app.js
+++ 1/app.js
@@ -6,8 +6,27 @@ class JeemaCoder extends React.Component {
             nomInput: '',
             emailInput: '',
             telInput: '',
-            etudiants:[]
+            etudiants: []
         };
+    }
+
+    //    methode ajout etudiants
+    addStudent = (e) => {
+        e.preventDefault()
+
+        const newStudent = {
+            prenom: this.state.prenomInput,
+            nom: this.state.nomInput,
+            mail: this.state.emailInput,
+            tel: this.state.telInput
+        }
+        this.setState({
+            etudiants: [...this.state.etudiants, newStudent],
+            prenomInput: "",
+            nomInput: "",
+            emailInput: "",
+            telInput: ""
+        })
     }
 
     render() {
@@ -23,9 +42,9 @@ class JeemaCoder extends React.Component {
                             <input type="text"
                                 className="form-control"
                                 value={this.state.prenomInput}
-                                onChange={(e)=>{
+                                onChange={(e) => {
                                     this.setState({
-                                        prenomInput:e.target.value
+                                        prenomInput: e.target.value
                                     })
                                 }}
                             />
@@ -36,7 +55,7 @@ class JeemaCoder extends React.Component {
                             <input type="text"
                                 className="form-control"
                                 value={this.state.nomInput}
-                                onChange={(e)=>{
+                                onChange={(e) => {
                                     this.setState({
                                         nomInput: e.target.value
                                     })
@@ -52,8 +71,8 @@ class JeemaCoder extends React.Component {
                             <input type="email"
                                 className="form-control"
                                 value={this.state.emailInput}
-                                onChange={(e)=>{
-                                     this.setState({
+                                onChange={(e) => {
+                                    this.setState({
                                         emailInput: e.target.value
                                     })
                                 }}
@@ -65,9 +84,9 @@ class JeemaCoder extends React.Component {
                             <input type="tel"
                                 className="form-control"
                                 value={this.state.telInput}
-                                onChange={(e)=>{
+                                onChange={(e) => {
                                     this.setState({
-                                        telInput:e.target.value
+                                        telInput: e.target.value
                                     })
                                 }}
                             />
@@ -77,10 +96,38 @@ class JeemaCoder extends React.Component {
                     <div className="d-flex justify-content-center">
 
                         <button type="submit" className="btn btn-dark mt-2 "
-                          
+                            onClick={this.addStudent}
                         >Envoyer</button>
                     </div>
                 </form>
+                <div className="mt-3">
+                    <table className="table mt-3">
+
+
+                        <thead>
+                            <tr>
+
+                                <th scope="col">Prenom</th>
+                                <th scope="col">Nom</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Telephone</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.etudiants.map((etudiant, index) => (
+                                <tr key={index} className="my-3">
+                                    <td>{etudiant.prenom}</td>
+                                    <td>{etudiant.nom}</td>
+                                    <td>{etudiant.mail}</td>
+                                    <td>{etudiant.tel}</td>
+                                    
+                                </tr>
+                            ))}
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
